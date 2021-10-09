257eb5374b642a54b38c1928c63d0c940258c933
李小拐 <2447473295@qq.com>
2021-10-09 12:13:23 +0800

    ptt

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,22 +1,15 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>4everland撸币教程(web3.0赛道，未来可期)</p>
-    <p>一：打开项目官网：<a href="https://hosting.4everland.org/#/?invite=OV9GGLIW" target="_blank">https://hosting.4everland.org/#/?invite=OV9GGLIW</a>,使用github账户登录4EVERLAND官网，github账号必须是9.8之前注册的,使用账号一键登录。</br><img src="../assets/11.png"></p>
+    <p>优质资源,不定期更新,10月1日国庆更新：绝版资源，女主已经被抓</p>
+    <p><img src="../assets/7.png"></p>
 
-    <p>二：访问活动页面：<a href="https://hosting.4everland.org/#/?invite=OV9GGLIW" target="_blank">https://hosting.4everland.org/#/?invite=OV9GGLIW</a>,点击new project创建一个项目</br><img src="../assets/3.jpg"></p>
+    <p><img src="../assets/9.jpg"></p>
 
-    <p>三：然后点击Connect to Git，从自己的Github库或者右侧4EVERLAND提供的模板中选择一个项目进行创建。</br><img src="../assets/4.jpg"></p>
+    <p><img src="../assets/10.jpg"></p>
 
-    <p>四：设置参数，系统会自动帮你识别框架，点击Deploy</br><img src="../assets/5.jpg"></p>
-
-    <p>五：部署完成</br><img src="../assets/6.jpg"></p>
-    
-    <p>（最后步骤完成可获得1000个4EVER）Web3.0版块后面应该会有爆发，不要放过任何一个羊毛</p>
     <p>
-    开始时间：9 月 15 日 0:00 (UTC)</br>
-    结束时间：10 月 7 日-10 月 14 日 不定时结束，尽量在7号前完成</br>
-    注意：必须要9.8号之前注册的github账号才能参与，奖励会在活动结束后发放
+    本站不定期更新资源，随缘
     </p>
   </div>
 </template>
